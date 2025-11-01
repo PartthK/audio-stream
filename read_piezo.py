35225e59501c0c495afc513dcf116afa58095c5e
Noah Hallberg <hallberg.noah7@gmail.com>
2025-11-01 14:59:22 -0400

    Add files via upload

--- read_piezo.py
+++ read_piezo.py
@@ -1,3 +1,4 @@
+import sys
 import time
 import board
 import busio
@@ -9,10 +10,27 @@ i2c = busio.I2C(board.SCL, board.SDA)
 ads = ADS.ADS1115(i2c)
 ads.data_rate = 860
 ads.gain = 1
-chan = AnalogIn(ads, 0)
-
+chan0 = AnalogIn(ads, 0)
+chan1 = AnalogIn(ads, 1)
+chan2 = AnalogIn(ads, 2)
+chan3 = AnalogIn(ads, 3)
+print(f"Start")
 while True:
-	v = abs(chan.voltage)
-	if v > 1.0:
-		print(f"MQ-135 Voltage: {v}V")
-	time.sleep(0.0005)
+	try:
+		v0 = abs(chan0.voltage)
+		v1 = abs(chan1.voltage)
+		v2 = abs(chan2.voltage)
+		v3 = abs(chan3.voltage)
+		if v0 > 0.01:
+			print(f"Voltage on A0: {v0:.3f}V")
+		if v1 > 0.01:
+			print(f"Voltage on A1: {v1:.3f}V")
+		if v2 > 0.01:
+			print(f"Voltage on A2: {v2:.3f}V")
+		if v3 > 0.01:
+			print(f"Voltage on A3: {v3:.3f}V")
+		time.sleep(0.005)
+	except KeyboardInterrupt:
+		sys.exit()
+	except:
+		continue
